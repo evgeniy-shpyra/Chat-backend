de0e1c0b65184dd48ef2df158c0dd8036ba27df0
evgeniy-shpyra <foksmix256@gmail.com>
2022-12-08 17:30:57 +0200

    fixed refresh

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.use(cookieParser())
 app.use(
     cors({
         credentials: true,
-        origin: [process.env.CLIENT_URL],
-        ENV
+        origin: [process.env.CLIENT_URL]
     })
 )
 app.use(fileUpload({}))
