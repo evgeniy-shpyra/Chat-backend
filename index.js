6cb97fb09fb0aadd60853973edd8b97fd04e457f
evgeniy-shpyra <foksmix256@gmail.com>
2022-12-09 16:35:23 +0200

    fixed connection to socket.io client

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = app.listen(port, () => {
 const io = socket(server, {
     cors: {
         // origin: [process.env.CLIENT_URL],
-        origin: ['http://localhost:3000'],
+        origin: ['https://chat-frontend-snowy.vercel.app'],
         credentials: true,
         methods: ['GET', 'POST'],
     },
