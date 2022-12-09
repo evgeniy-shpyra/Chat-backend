f50c65e50ce7e8f37e58b61868d10188487d40f7
evgeniy-shpyra <foksmix256@gmail.com>
2022-12-09 23:45:40 +0200

    fixed connection to socket.io client 14

--- index.js
+++ index.js
@@ -43,8 +43,17 @@ const io = socket(server, {
 
 global.onlineUsers = new Map()
 
+io.engine.on("connection_error", (err) => {
+    console.log(err.req);      // the request object
+    console.log(err.code);     // the error code, for example 1
+    console.log(err.message);  // the error message, for example "Session ID unknown"
+    console.log(err.context);  // some additional error context
+  });
+
 io.on('connection', (socket) => {
     global.chatSocket = socket
+
+    
 
     socket.on('online:add', (userId) => {
         onlineUsers.set(userId, socket.id)
